e05b1e7ce40d1195e29f6419e34c24f934e6d31b
Oriel <orianafarrugia3@gmail.com>
2020-06-12 16:07:31 -0500

    Spreadsheet friendly output & verbose mode

--- recordkeeper.sh
+++ recordkeeper.sh
@@ -10,6 +10,7 @@ WRITEABLE="1"
 PROBLEMS=""
 TARGETFILE=""
 OUTPUT=""
+VERBOSE=0
 HELP="Help screen\n"
 OPTIND=1
 
@@ -36,15 +37,40 @@ fi
 ###########################################
 # --) Targets file
 
-while getopts "h?t:" opt; do
+while getopts "h?t:o:v" opt; do
 	case "$opt" in 
 	h|\?) printf "$HELP"; exit 0;;
 	t)	TARGETFILE=$OPTARG;;
 	o)	OUTPUT=$OPTARG;;
+	v)	VERBOSE=1;;
 	esac
 done
 
+###########################################
+#-----------) Perform Lookup (------------#
+###########################################
+
+if ! [ $OUTPUT ]; then
 
 while IFS="" read -r domain || [ -n "$domain" ]; do
-  host -a $domain | awk '{ if (!a && $0~/ANSWER SECTION/) {a=1;next;} } a' | grep $domain | awk 
+
+	if [ $VERBOSE ]; then
+		printf "[+] Performing lookup on $domain \n"
+	fi
+	
+	host -a $domain | awk '{ if (!a && $0~/ANSWER SECTION/) {a=1;next;} } a' | grep $domain | awk '{ printf $4 "\t" $1 "\t" $5 "\t" $6 "\n"}'
+
 done < $TARGETFILE
+fi
+
+if [ $OUTPUT ]; then
+
+while IFS="" read -r domain || [ -n "$domain" ]; do
+
+	if [ $VERBOSE ]; then
+		printf "[+] Performing lookup on $domain \n"
+	fi
+
+	host -a $domain | awk '{ if (!a && $0~/ANSWER SECTION/) {a=1;next;} } a' | grep $domain | awk '{ printf $4 "\t" $1 "\t" $5 "\t" $6 "\n"}' >> $OUTPUT
+done < $TARGETFILE
+fi
